616df3e119089e14c0d59f59f3c34a42a09e5b5b
dufour thibault <thib@rhea.local>
2023-08-08 20:47:34 +0200

    initial version

---
+++ script/check_updates.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# script to pring version for each operator being used
+# create an ouput to a date file 
+responsefile=$1
+osh_version=$2
+cat responsfile.txt | while read catalog operator channel
+do 
+	version=$(oc-mirror list operators --catalog=registry.redhat.io/redhat/${catalog}:${osh_version} --package=${package} --channel=${channel})
+	echo ${version}
+done
